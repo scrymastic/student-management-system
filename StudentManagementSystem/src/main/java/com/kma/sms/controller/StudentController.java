9f2e3f02591344bbadfa8d23ec4e70b685a6e89f
scrymastic <AT180142@actvn.edu.vn>
2023-11-28 01:29:57 +0700

    Initial commit

---
+++ StudentManagementSystem/src/main/java/com/kma/sms/controller/StudentController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kma.sms.controller;
+
+/**
+ *
+ * @author author's_name_goes_here
+ */
+public class StudentController {
+    
+}
